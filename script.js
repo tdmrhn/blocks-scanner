cbc1d7fa3e150da60c03d1f4845e9955133e9525
Taha Demirhan <dmrhn@gmx.com>
2024-03-23 19:17:21 +0300

    Totally rewritten with class

--- script.js
+++ script.js
@@ -1,92 +1,107 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const toggleTab = (tab) => {
-        event.preventDefault();
-        const targetId = tab.getAttribute("href").substring(1);
-        document.querySelectorAll(".nav-tab-wrapper a").forEach(t => t.classList.toggle("nav-tab-active", t === tab));
-        document.querySelectorAll(".tab-content").forEach(content => content.classList.toggle("active", content.id === targetId));
-    };
+    class BlocksScanner {
+        constructor() {
+            this.tabWrapper = document.querySelectorAll(".nav-tab-wrapper a");
+            this.tabContent = document.querySelectorAll(".tab-content");
+            this.filterInputs = document.querySelectorAll('.tab-content .dhn-filter, .tab-content #block-dropdown');
+            this.initialize();
+        }
 
-    document.querySelectorAll(".nav-tab-wrapper a").forEach(tab => tab.addEventListener("click", () => toggleTab(tab)));
-    document.querySelector(".nav-tab-wrapper a").click();
+        toggleTab(tab) {
+            event.preventDefault();
+            const targetId = tab.getAttribute("href").substring(1);
+            this.tabWrapper.forEach(t => t.classList.toggle("nav-tab-active", t === tab));
+            this.tabContent.forEach(content => content.classList.toggle("active", content.id === targetId));
+        }
 
-    const updateRowCountAndFilter = () => {
-        document.querySelectorAll('.tab-content').forEach(tabContent => {
-            const filterInput = tabContent.querySelector('.dhn-filter');
-            const blockDropdown = document.getElementById('block-dropdown');
-            const tableRows = tabContent.querySelectorAll('tbody tr');
-            const rowCountElement = tabContent.querySelector('.row-count');
+        updateRowCountAndFilter() {
+            this.tabContent.forEach(tabContent => {
+                const filterInput = tabContent.querySelector('.dhn-filter');
+                const blockDropdown = tabContent.querySelector('#block-dropdown');
+                const tableRows = tabContent.querySelectorAll('tbody tr');
+                const rowCountElement = tabContent.querySelector('.row-count');
 
-            const filterText = filterInput.value.trim().toLowerCase();
-            const selectedBlock = blockDropdown.value.toLowerCase();
+                const filterText = filterInput.value.trim().toLowerCase();
+                const selectedBlock = blockDropdown.value.toLowerCase();
 
-            let visibleRowCount = 0;
+                let visibleRowCount = 0;
 
-            tableRows.forEach(row => {
-                const blockName = row.cells[1].textContent.trim().toLowerCase();
-                const shouldDisplay = (selectedBlock === "all" || blockName.includes(selectedBlock)) && row.textContent.toLowerCase().includes(filterText);
-                row.style.display = shouldDisplay ? "" : "none";
-                if (shouldDisplay) visibleRowCount++;
+                tableRows.forEach(row => {
+                    const blockName = row.cells[1].textContent.trim().toLowerCase();
+                    const shouldDisplay = (selectedBlock === "all" || blockName.includes(selectedBlock)) && row.textContent.toLowerCase().includes(filterText);
+                    row.style.display = shouldDisplay ? "" : "none";
+                    if (shouldDisplay) visibleRowCount++;
+                });
+
+                rowCountElement.textContent = visibleRowCount;
+            });
+        }
+
+        sortGrid(grid, colNum, type, sortOrder) {
+            const tbody = grid.querySelector('tbody');
+            const rowsArray = Array.from(tbody.rows);
+            let compare;
+
+            switch (type) {
+                case 'number':
+                    compare = (rowA, rowB) => Number(rowA.cells[colNum].innerHTML) - Number(rowB.cells[colNum].innerHTML);
+                    break;
+                case 'string':
+                    compare = (rowA, rowB) => {
+                        const valueA = rowA.cells[colNum].textContent.trim().toLowerCase();
+                        const valueB = rowB.cells[colNum].textContent.trim().toLowerCase();
+                        return valueA.localeCompare(valueB, undefined, {numeric: true});
+                    };
+                    break;
+                case 'date':
+                    compare = (rowA, rowB) => new Date(rowA.cells[colNum].innerHTML) - new Date(rowB.cells[colNum].innerHTML);
+                    break;
+            }
+
+            if (sortOrder === 'asc') {
+                rowsArray.sort(compare);
+            } else {
+                rowsArray.sort((a, b) => compare(b, a)); 
+            }
+
+            tbody.innerHTML = ''; 
+
+            rowsArray.forEach(row => tbody.appendChild(row));
+        }
+
+        initializeTableSorting() {
+            this.tabContent.forEach(tabContent => {
+                const tables = tabContent.querySelectorAll('table');
+                tables.forEach(grid => {
+                    grid.onclick = (e) => {
+                        if (e.target.tagName !== 'TH') return;
+
+                        const th = e.target;
+                        const colIndex = th.cellIndex;
+                        let currentSortOrder = th.dataset.sortOrder || 'asc';
+
+                        currentSortOrder = currentSortOrder === 'asc' ? 'desc' : 'asc';
+                        th.dataset.sortOrder = currentSortOrder;
+
+                        this.sortGrid(grid, colIndex, th.dataset.type, currentSortOrder);
+                    };
+                });
+            });
+        }
+
+        initialize() {
+            this.tabWrapper.forEach(tab => tab.addEventListener("click", () => this.toggleTab(tab)));
+            this.tabWrapper[0].click();
+
+            this.filterInputs.forEach(element => {
+                element.addEventListener("input", () => this.updateRowCountAndFilter());
+                element.addEventListener("change", () => this.updateRowCountAndFilter());
             });
 
-            rowCountElement.textContent = visibleRowCount;
-        });
-    };
+            this.updateRowCountAndFilter();
+            this.initializeTableSorting();
+        }
+    }
 
-    document.querySelectorAll('.tab-content .dhn-filter, .tab-content #block-dropdown').forEach(element => {
-        element.addEventListener("input", updateRowCountAndFilter);
-        element.addEventListener("change", updateRowCountAndFilter);
-    });
-
-    updateRowCountAndFilter();
-
-    const initializeTableSorting = () => {
-        document.querySelectorAll('table').forEach(grid => {
-            grid.onclick = (e) => {
-                if (e.target.tagName !== 'TH') return;
-
-                const th = e.target;
-                const colIndex = th.cellIndex;
-                let currentSortOrder = th.dataset.sortOrder || 'asc';
-
-                currentSortOrder = currentSortOrder === 'asc' ? 'desc' : 'asc';
-                th.dataset.sortOrder = currentSortOrder;
-
-                sortGrid(colIndex, th.dataset.type, currentSortOrder);
-            };
-
-            const sortGrid = (colNum, type, sortOrder) => {
-                const tbody = grid.querySelector('tbody');
-                const rowsArray = Array.from(tbody.rows);
-                let compare;
-
-                switch (type) {
-                    case 'number':
-                        compare = (rowA, rowB) => Number(rowA.cells[colNum].innerHTML) - Number(rowB.cells[colNum].innerHTML);
-                        break;
-                    case 'string':
-                        compare = (rowA, rowB) => {
-                            const valueA = rowA.cells[colNum].textContent.trim().toLowerCase();
-                            const valueB = rowB.cells[colNum].textContent.trim().toLowerCase();
-                            return valueA.localeCompare(valueB, undefined, {numeric: true});
-                        };
-                        break;
-                    case 'date':
-                        compare = (rowA, rowB) => new Date(rowA.cells[colNum].innerHTML) - new Date(rowB.cells[colNum].innerHTML);
-                        break;
-                }
-
-                if (sortOrder === 'asc') {
-                    rowsArray.sort(compare);
-                } else {
-                    rowsArray.sort((a, b) => compare(b, a)); 
-                }
-
-                tbody.innerHTML = ''; 
-
-                rowsArray.forEach(row => tbody.appendChild(row));
-            };
-        });
-    };
-
-    initializeTableSorting();
+    const blocksScanner = new BlocksScanner();
 });
