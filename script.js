a98d8a40bd01985cfe3ce1ad7cd877740d236dd1
Taha Demirhan <dmrhn@gmx.com>
2024-03-25 15:47:46 +0300

    Change dropdown to checkboxes
    Changed dropdown to checkboxes with multi-select feature

--- script.js
+++ script.js
@@ -1,107 +1,112 @@
 document.addEventListener("DOMContentLoaded", () => {
-    class BlocksScanner {
-        constructor() {
-            this.tabWrapper = document.querySelectorAll(".nav-tab-wrapper a");
-            this.tabContent = document.querySelectorAll(".tab-content");
-            this.filterInputs = document.querySelectorAll('.tab-content .dhn-filter, .tab-content #block-dropdown');
-            this.initialize();
-        }
+	class BlocksScanner {
+		constructor() {
+			this.tabWrapper = document.querySelectorAll(".nav-tab-wrapper a");
+			this.tabContent = document.querySelectorAll(".tab-content");
+			this.filterInputs = document.querySelectorAll('.tab-content .dhn-filter, .tab-content .block-checkbox');
+			this.initialize();
+		}
 
-        toggleTab(tab) {
-            event.preventDefault();
-            const targetId = tab.getAttribute("href").substring(1);
-            this.tabWrapper.forEach(t => t.classList.toggle("nav-tab-active", t === tab));
-            this.tabContent.forEach(content => content.classList.toggle("active", content.id === targetId));
-        }
+		toggleTab(tab) {
+			event.preventDefault();
+			const targetId = tab.getAttribute("href").substring(1);
+			this.tabWrapper.forEach(t => t.classList.toggle("nav-tab-active", t === tab));
+			this.tabContent.forEach(content => content.classList.toggle("active", content.id === targetId));
+		}
 
-        updateRowCountAndFilter() {
-            this.tabContent.forEach(tabContent => {
-                const filterInput = tabContent.querySelector('.dhn-filter');
-                const blockDropdown = tabContent.querySelector('#block-dropdown');
-                const tableRows = tabContent.querySelectorAll('tbody tr');
-                const rowCountElement = tabContent.querySelector('.row-count');
+		updateRowCountAndFilter() {
+			this.tabContent.forEach(tabContent => {
+				const filterInput = tabContent.querySelector('.dhn-filter');
+				const blockCheckboxes = tabContent.querySelectorAll('.block-checkbox');
+				const tableRows = tabContent.querySelectorAll('tbody tr');
+				const rowCountElement = tabContent.querySelector('.row-count');
 
-                const filterText = filterInput.value.trim().toLowerCase();
-                const selectedBlock = blockDropdown.value.toLowerCase();
+				const filterText = filterInput.value.trim().toLowerCase();
+				const selectedBlocks = Array.from(blockCheckboxes)
+					.filter(checkbox => checkbox.checked)
+					.map(checkbox => checkbox.value.toLowerCase());
 
-                let visibleRowCount = 0;
+				let visibleRowCount = 0;
 
-                tableRows.forEach(row => {
-                    const blockName = row.cells[1].textContent.trim().toLowerCase();
-                    const shouldDisplay = (selectedBlock === "all" || blockName === selectedBlock) && row.textContent.toLowerCase().includes(filterText);
-                    row.style.display = shouldDisplay ? "" : "none";
-                    if (shouldDisplay) visibleRowCount++;
-                });
+				tableRows.forEach(row => {
+					const blockName = row.cells[1].textContent.trim().toLowerCase();
+					const shouldDisplay = (selectedBlocks.length === 0 || selectedBlocks.includes(blockName)) && row.textContent.toLowerCase().includes(filterText);
+					row.style.display = shouldDisplay ? "" : "none";
+					if (shouldDisplay) visibleRowCount++;
+				});
 
-                rowCountElement.textContent = visibleRowCount;
-            });
-        }
+				rowCountElement.textContent = visibleRowCount;
+			});
+		}
 
-        sortGrid(grid, colNum, type, sortOrder) {
-            const tbody = grid.querySelector('tbody');
-            const rowsArray = Array.from(tbody.rows);
-            let compare;
 
-            switch (type) {
-                case 'number':
-                    compare = (rowA, rowB) => Number(rowA.cells[colNum].innerHTML) - Number(rowB.cells[colNum].innerHTML);
-                    break;
-                case 'string':
-                    compare = (rowA, rowB) => {
-                        const valueA = rowA.cells[colNum].textContent.trim().toLowerCase();
-                        const valueB = rowB.cells[colNum].textContent.trim().toLowerCase();
-                        return valueA.localeCompare(valueB, undefined, {numeric: true});
-                    };
-                    break;
-                case 'date':
-                    compare = (rowA, rowB) => new Date(rowA.cells[colNum].innerHTML) - new Date(rowB.cells[colNum].innerHTML);
-                    break;
-            }
+		sortGrid(grid, colNum, type, sortOrder) {
+			const tbody = grid.querySelector('tbody');
+			const rowsArray = Array.from(tbody.rows);
+			let compare;
 
-            if (sortOrder === 'asc') {
-                rowsArray.sort(compare);
-            } else {
-                rowsArray.sort((a, b) => compare(b, a)); 
-            }
+			switch (type) {
+				case 'number':
+					compare = (rowA, rowB) => Number(rowA.cells[colNum].innerHTML) - Number(rowB.cells[colNum].innerHTML);
+					break;
+				case 'string':
+					compare = (rowA, rowB) => {
+						const valueA = rowA.cells[colNum].textContent.trim().toLowerCase();
+						const valueB = rowB.cells[colNum].textContent.trim().toLowerCase();
+						return valueA.localeCompare(valueB, undefined, {
+							numeric: true
+						});
+					};
+					break;
+				case 'date':
+					compare = (rowA, rowB) => new Date(rowA.cells[colNum].innerHTML) - new Date(rowB.cells[colNum].innerHTML);
+					break;
+			}
 
-            tbody.innerHTML = ''; 
+			if (sortOrder === 'asc') {
+				rowsArray.sort(compare);
+			} else {
+				rowsArray.sort((a, b) => compare(b, a));
+			}
 
-            rowsArray.forEach(row => tbody.appendChild(row));
-        }
+			tbody.innerHTML = '';
 
-        initializeTableSorting() {
-            this.tabContent.forEach(tabContent => {
-                const tables = tabContent.querySelectorAll('table');
-                tables.forEach(grid => {
-                    grid.onclick = (e) => {
-                        if (e.target.tagName !== 'TH') return;
+			rowsArray.forEach(row => tbody.appendChild(row));
+		}
 
-                        const th = e.target;
-                        const colIndex = th.cellIndex;
-                        let currentSortOrder = th.dataset.sortOrder || 'asc';
+		initializeTableSorting() {
+			this.tabContent.forEach(tabContent => {
+				const tables = tabContent.querySelectorAll('table');
+				tables.forEach(grid => {
+					grid.onclick = (e) => {
+						if (e.target.tagName !== 'TH') return;
 
-                        currentSortOrder = currentSortOrder === 'asc' ? 'desc' : 'asc';
-                        th.dataset.sortOrder = currentSortOrder;
+						const th = e.target;
+						const colIndex = th.cellIndex;
+						let currentSortOrder = th.dataset.sortOrder || 'asc';
 
-                        this.sortGrid(grid, colIndex, th.dataset.type, currentSortOrder);
-                    };
-                });
-            });
-        }
+						currentSortOrder = currentSortOrder === 'asc' ? 'desc' : 'asc';
+						th.dataset.sortOrder = currentSortOrder;
 
-        initialize() {
-            this.tabWrapper.forEach(tab => tab.addEventListener("click", () => this.toggleTab(tab)));
-            this.tabWrapper[0].click();
+						this.sortGrid(grid, colIndex, th.dataset.type, currentSortOrder);
+					};
+				});
+			});
+		}
 
-            this.filterInputs.forEach(element => {
-                element.addEventListener("input", () => this.updateRowCountAndFilter());
-                element.addEventListener("change", () => this.updateRowCountAndFilter());
-            });
+		initialize() {
+			this.tabWrapper.forEach(tab => tab.addEventListener("click", () => this.toggleTab(tab)));
+			this.tabWrapper[0].click();
 
-            this.updateRowCountAndFilter();
-            this.initializeTableSorting();
-        }
-    }
+			this.filterInputs.forEach(element => {
+				element.addEventListener("input", () => this.updateRowCountAndFilter());
+				element.addEventListener("change", () => this.updateRowCountAndFilter());
+			});
 
-    const blocksScanner = new BlocksScanner();
+			this.updateRowCountAndFilter();
+			this.initializeTableSorting();
+		}
+	}
+
+	const blocksScanner = new BlocksScanner();
 });
